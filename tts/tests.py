882b94d112bbb10c278f8f21bf7fd96d95bc2ba7
Rohini Singh Senger <rohinisinghsengar@gmail.com>
2026-01-08 22:30:58 +0530

    Add basic tests

--- tts/tests.py
+++ tts/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HealthTest(TestCase):
+    def test_home_page(self):
+        resp = self.client.get("/")
+        self.assertEqual(resp.status_code, 200)
